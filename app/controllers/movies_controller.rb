9de7f581d007fba8d406dd777e700cf668ff1a81
rlatullip <rlatullip@qmail.qcc.edu>
2015-07-25 05:27:32 -0400

    Section 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,9 @@ class MoviesController < ApplicationController
     id = params[:id] #retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
   end
+  
+  # Section 4.5 Demos
+  # raise params.inspect (commented out after trying)
+  logger.debug('testing, testing, 1...2...3...testing')
 
 end
