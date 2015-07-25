c29f4b0e0990761d74804b3c01f5c531925b93ee
rlatullip <rlatullip@qmail.qcc.edu>
2015-07-25 05:47:35 -0400

    Section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
   end
   
+  # Section 4.6
+  def new
+  	# default: render 'new' template
+  end
+  
   # Section 4.5 Demos
   # raise params.inspect (commented out after trying)
   logger.debug('testing, testing, 1...2...3...testing')
